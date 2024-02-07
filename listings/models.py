f6a07de7af3af1a85e9aba0e0a2e8450fd935267
Jj RamOS <fakemail@fakeDomain.com>
2024-02-06 17:48:31 -0800

    Create regsiter form and view

--- listings/models.py
+++ listings/models.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
 
 class Listing(models.Model):
     seller = models.ForeignKey(User, related_name='listings', on_delete=models.CASCADE)
--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from django.contrib import messages
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')  # Assuming you have a URL named 'login'
+    else:
+        form = UserRegisterForm()
+    return render(request, 'listings/register.html', {'form': form})
