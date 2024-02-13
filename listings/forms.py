9f127aba0e85db6699e269ca5e6435d8681af7b5
Jj RamOS <fakemail@fakeDomain.com>
2024-02-12 18:33:36 -0800

    Add style to register, login, send message forms

--- listings/forms.py
+++ listings/forms.py
@@ -9,6 +9,15 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2', 'first_name', 'last_name']
 
+        widgets = {
+            'username': forms.TextInput(attrs={'class':'form-control'}),
+            'email': forms.EmailInput(attrs={'class':'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class':'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class':'form-control'}),
+            'first_name': forms.TextInput(attrs={'class':'form-control'}),
+            'last_name': forms.TextInput(attrs={'class':'form-control'})  
+        }
+
 class ListingForm(forms.ModelForm):
     class Meta:
         model = Listing
